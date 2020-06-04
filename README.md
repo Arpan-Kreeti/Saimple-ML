bd488cdf2e8a37f56ff6632af468ab481d5a5036
Arpan-Kreeti <arpan.ghoshal@kreeti.com>
2020-06-04 11:38:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SimpleMl
+
+Implementation of ML algorithms in Elixir, for learning and educational purposes.
---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule SimpleMl.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :simple_ml,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:csv, "~> 2.3"},
+      {:matrex, "~> 0.6.8"}
+    ]
+  end
+end
